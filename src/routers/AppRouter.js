ee3060bdbe47cba51248b192284ecd6096b21d4c
Frederik Brandt <frederiksbrandt@gmail.com>
2021-01-25 08:43:56 +0100

    Minor error in approuter file

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -4,7 +4,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
 import AddExpensePage from '../components/AddExpensePage';
 import EditExpensePage from '../components/EditExpensePage';
-import HelpPage from '../components/Helppage';
+import HelpPage from '../components/HelpPage';
 import NotFoundPage from '../components/NotFoundPage';
 import Header from '../components/Header';
 
